8ecc4591a3ae9430a4741ae5c26d9cde512e632e
Myeisha Swain <zse0296@gmail.com>
2022-10-06 20:34:02 -0400

    added notes and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler 
+
+Work Day is a Hour by Hour work day planner for busy individuals.
+It is streamlined in the sense of it is only for the current day.
+It is built around the current 9-5 work week.
+
+It uses JavaScripts Date Picker functionality over Moment.JS which is no longer supported.
+ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,33 @@
 
 $(document).ready(function(){
 
+    //obtains current date and appends it to the page
     var currentDate = new Date().toDateString();
     $("#currentDay").text(currentDate);
 
     var currentHour = new Date().getHours();
 
     for(var i=9; i <18; i++){
+        //Var to control color of the time slot with if statements
         var colorKey = "";
         if(i<currentHour){
             colorKey = "past"
         };
+        if(i>currentHour){
+            colorKey = "future"
+        };
+        if(i == currentHour){
+            colorKey = "present"
+        };
+        //end var and statements to control color of timeslot
+
+        //Dynamically creates elements on the page
         var row = $("<div>").addClass("row time-block").attr("id", i);
         var hour = $("<div>").addClass("col-2 hour").text(i);
         var textArea = $("<textarea>").addClass("col-8 description " + colorKey).val(localStorage.getItem(i));
         var icon = $("<i>").addClass("fas fa-save");
+        
+        //var and function to save to local storage via the button
         var saveButton =$("<button>").attr("id",i).addClass("col-2 saveBtn btn btn-primary")
         .click(function(){
             var hourKey = $(this).attr("id");
